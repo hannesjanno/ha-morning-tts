220200f9f13b223738b282ae5fa9054e0a1e0381
hannesjanno <hannesjanno@hotmail.com>
2026-09-02 21:29:31 +0300

    Enable guest room robot vacuum overlay

--- home-os/src/main.jsx
+++ home-os/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import { installFurnitureOverlay } from './furnitureOverlay.js';
+import { installVacuumOverlay } from './vacuumOverlay.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,3 +12,4 @@ createRoot(document.getElementById('root')).render(
 );
 
 installFurnitureOverlay();
+installVacuumOverlay();
